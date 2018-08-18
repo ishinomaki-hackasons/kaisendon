f7bf2f385cc603edb4955901fc04859f98ced756
ShunUehara <s.uehara@zooops-japan.co.jp>
2018-08-18 21:33:03 +0900

    update Drone テスト
    made by Migita6

--- drone/droneMove.js
+++ drone/droneMove.js
@@ -1,8 +1,34 @@
 
 // ずっと静かな場合：youOkay（このままでいいの？）
 moves.youOkay = function(drone) {
-    // ちょっと下に落とす（落ちそうな感じ、ヒヤッとさせたい）
-    // ドローンを下に落とす処理を記載
+    // 前後に振る（挑発する感じ、煽りたい）
+    setTimeout(function() {
+      clockwise(10);
+      up(3);
+    }, 1000);
+
+// setTimeout(function() {
+//   clockwise(10);
+//   down(3);
+// }, 3000);
+//
+//     setTimeout(function() {
+//     stop();
+//   }, 5000);
 }
 
-// more code・・・
+// 静かな状態から騒がしくなった場合：letsGo（いくぞ！）
+moves.letsGo = function(drone) {
+    // 回転させる（横回転、嬉しい動き）
+    counterClockwise(10)
+    clockwise(10)
+    stop();
+}
+
+// 騒がしい状態から静かになった場合：whatsUp（おいおいどうした）
+moves.whatsUp = function(drone) {
+    // ちょっと下に落とす（落ちそうな感じ、ヒヤッとさせたい）
+    //お笑いでこける感じ
+    laud();
+    stop();
+}
